4c3bc832016bd9bc1dff1ff49187600f64a532a0
simon.chang <simonwell.ios@gmail.com>
2020-10-26 14:24:21 +0800

    upload SwiftUI_ExpandingList

---
+++ SwiftUI_ExpandingList/SwiftUI_ExpandingList/SwiftUI_ExpandingListApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ExpandingListApp.swift
+//  SwiftUI_ExpandingList
+//
+//  Created by simon.chang on 2020/10/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ExpandingListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
